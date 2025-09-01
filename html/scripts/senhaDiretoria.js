4bbf02dd496e95476e6ba66aacfe6654879623b3
VitRicci <202402576721@alunos.estacio.br>
2025-09-01 12:38:51 -0300

    Update senhaDiretoria.js

--- html/scripts/senhaDiretoria.js
+++ html/scripts/senhaDiretoria.js
@@ -4,7 +4,7 @@ function mostrarSenha() {
 
 function verificarSenha() {
     const senha = document.getElementById('inputSenha').value;
-    fetch('/api/login', {
+    fetch('https://projetomuseu.vercel.app/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ senha })
